c3ef249e9cd24d9f70428fa23695f231a94120a6
guanyi <guanyifang@hotmail.com>
2015-04-02 05:14:48 -0700

    Completed upload and delete pictures. Only has image type validation left.

--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -4,8 +4,8 @@
         var row = document.getElementsByName(rowName);
         row.value = "";
     }
-
 </script>
+
 @section('content')
     <h1>{{$user->id}}</h1>
     {{ Form::open(array('url' => 'logout')) }}
@@ -14,7 +14,7 @@
 
     <hr>
 
-    {{ Form::open(array('url' => 'profile')) }}
+    {{ Form::open(array('url' => 'profile', 'files' => true)) }}
     <h1>Notes</h1>
 
     <p>
@@ -26,7 +26,7 @@
         {{ Form::textarea('tbd', $user->tbd) }}
     </p>
 
-    <h1>Websites. Click to open</h1>
+    <h1>Websites</h1>
 
     <table border='0' class='col-lg-12'>
         <tr class="">
@@ -78,7 +78,44 @@
         </tr>
     </table>
 
+    <h1>Images</h1>
+
+    <p>
+        {{ "Image 1" . Form::file('image1') . "Check and save to delete  " . Form::checkbox('deleteImage1', 'v1', false) . "<br><br>" }}
+        {{ "Image 2" . Form::file('image2') . "Check and save to delete  " . Form::checkbox('deleteImage2', 'v2', false) . "<br><br>" }}
+        {{ "Image 3" . Form::file('image3') . "Check and save to delete  " . Form::checkbox('deleteImage3', 'v3', false) . "<br><br>" }}
+        {{ "Image 4" . Form::file('image4') . "Check and save to delete  " . Form::checkbox('deleteImage4', 'v4', false) . "<br><br>" }}
+    </p>
+
     <p>{{ Form::submit('Save')}}</p>
+
+    <?php
+        $user = Session::get('user');
+
+        if( $user->image1 != null ) {
+            $imagepath = "data:image/jpeg;base64,$user->image1";
+            echo "<br>Image 1<br>";
+            echo "<img src=$imagepath >";
+        }
+
+        if( $user->image2 != null ) {
+            echo "<br>Image 2<br>";
+            $imagepath = "data:image/jpeg;base64,$user->image2";
+            echo "<img src=$imagepath >";
+        }
+
+        if( $user->image3 != null ) {
+            echo "<br>Image 3<br>";
+            $imagepath = "data:image/jpeg;base64,$user->image3";
+            echo "<img src=$imagepath >";
+        }
+
+        if( $user->image4 != null ) {
+            echo "<br>Image 4<br>";
+            $imagepath = "data:image/jpeg;base64,$user->image4";
+            echo "<img src=$imagepath >";
+        }
+    ?>
 
     {{ Form::close() }}
 
